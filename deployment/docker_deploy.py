18ed7b91c7bce563af59f4da30e5900bb4349881
hopeJRD <jordanh0pe@pm.me>
2023-11-25 18:46:28 +0530

    docker deployment api

--- deployment/docker_deploy.py
+++ deployment/docker_deploy.py
@@ -19,17 +19,18 @@ def deploy_docker_container(image_name, service_name, replicas, ports_mapping, e
     service = client.services.create(**service_options)
 
     print(f"Service {service_name} deployed with ID {service.id}")
+    return service.id
 
 def list_docker_containers():
     client = docker.from_env()
     containers = client.containers.list()
     return containers
 
-def stop_docker_container(container_name):
+def stop_docker_container(deployment_id):
     client = docker.from_env()
-    container = client.containers.get(container_name)
+    container = client.containers.get(deployment_id)
     container.stop()
-    print(f"Container {container_name} stopped.")
+    print(f"Container with Deployment ID : {deployment_id} stopped.")
 
 
 def remove_docker_container(container_name):
@@ -43,3 +44,15 @@ def start_docker_container(container_name):
     container = client.containers.get(container_name)
     container.start()
     print(f"Container {container_name} started.")
+
+
+def get_deployment_status(deployment_id):
+    try:
+        client = docker.from_env()
+        container = client.containers.get(deployment_id)
+        status = container.status
+        return status
+    except docker.errors.NotFound:
+        return f"Container with name `{deployment_id}` not found."
+    except docker.errors.APIError as e:
+        return f'Error while getting container status : {e}'
